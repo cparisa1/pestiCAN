72c75af362add0636c1bab07b4641d0e14657acb
Parisa <parisachandrasekhar@gmail.com>
2019-06-03 22:48:03 -0400

    server changes

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
-/* //Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/dist/medizon-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname));
+res.sendFile(path.join(__dirname+'/dist/medizon-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); */
-
-
-const express = require('express');
-const http = require('http');
-
-const app = express();
-
-const port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname));
-
-const server = http.createServer(app);
-
-server.listen(port, ()=> console.log("Running..."));
+app.listen(process.env.PORT || 8080);
