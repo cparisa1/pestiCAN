f99ec0b7fe08745f51034b76ec92134dfc28befb
Parisa <parisachandrasekhar@gmail.com>
2020-03-12 15:20:32 -0400

    Modifications

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,6 +1,6 @@
 /* For Development API URL */
-//export const API_URL = 'http://localhost:5000';
+export const API_URL = 'http://localhost:5000';
 
 
 /* For Production API URL */
-export const API_URL = 'http://99.79.129.247';
+//export const API_URL = 'http://99.79.129.247';
